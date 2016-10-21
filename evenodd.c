936ce295f41a6aa42ca92542acdd764b8b17afcc
Kaid02 <kabirS317@gmail.com>
2016-10-21 11:44:54 +0545

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,99 @@
+/*
+Student Name : Kabir D Shrestha 
+Subject : Programming Fundamental
+Lab No. : 05
+Program : Write a program to print odd and even numbers using if else
+Date : 2016-10-21
+*/
+#include<stdio.h>
+	int main(){
+		int a;
+		printf("Enter the number");
+		scanf("%d",&a);
+		
+		if (a%2==0)
+		{
+			printf("\n%d number is even",a);
+		}
+		else {
+			printf("\n%d number is odd",a);
+		}
+		return 0;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
